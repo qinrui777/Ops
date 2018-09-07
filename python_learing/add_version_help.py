126ea83cf4bdc89489dc71818eff04fb85da429e
qinrui <771963601@qq.com>
2018-09-07 14:41:22 +0800

    add add_version_help.py

---
+++ python_learing/add_version_help.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python 
+import sys
+
+'''
+- add version command
+- add help command
+- add sub command e.g: run_test.py run_demo1 
+'''
+
+Version = ' Version:1.0.0   Time: 2018/09/07'
+
+def get_name():
+    print "print name..."
+
+def get_age():
+    print "print age..."
+
+if __name__== "__main__":
+
+    if sys.argv[1].startswith('--'):
+        option = sys.argv[1][2:]
+        if option == 'version':
+            print Version
+        elif option == 'help':
+            print '''  \t    aws get_ec2
+            aws get_subnet
+            --version : Prints the version number
+            --help     : Display this help'''
+        else:
+            print "Unknow option.Please check your syntax carefully."
+    else:
+        if sys.argv[1] == 'get_age':
+            get_age()
+        elif sys.argv[1] == 'get_name':
+            get_name()
+        else:
+            print "Unknow option.."
--- python_learing/sys_args_demo1.py
+++ python_learing/sys_args_demo1.py
@@ -1,10 +1,27 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python 
 import sys
-##类型
-print type(sys.argv)
+
+#sys.argv[]是用来获取命令行参数的
+
+###类型
+#print type(sys.argv)
 ##长度
-print len(sys.argv)
+#print len(sys.argv)
 ##第一个参数
-print sys.argv[0]
+#print sys.argv[0]
 ##第二个参数
-print sys.argv[1]
+#print sys.argv[1]
+
+def get_name():
+    print "print name..."
+
+def get_age():
+    print "print age..."
+
+if __name__== "__main__":
+    print "===being==="
+    get_name()
+    if sys.argv[1].startswith('--'):
+        option = sys.argv[1]
+        print option
